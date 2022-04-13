1f48c0b043ffa317fea2a2177d70f476574a838f
Juan David Orozco <ing.ele2018@gmail.com>
2022-04-13 00:41:02 -0500

    renderizacion JSON - L1U4

--- src/AppWeb.vue
+++ src/AppWeb.vue
@@ -6,14 +6,15 @@
     <div id="content">
       
       <noticia-componente
-        v-for='(noticia,index) in Noticias'
-        :fecha='noticia.publishedAt'
+        v-for='(noticia,index) in Noticias' 
+        :fecha='noticia.publishedAt' 
         :titulo='noticia.title'
         :imagen='noticia.urlToImage'
         :detalle='noticia.content'
         :url='noticia.url'
         :fuente='noticia.source.name'
-        :key='index'>
+        :key='index'
+      >
       </noticia-componente>
     
     </div>
@@ -45,123 +46,3 @@ export default {
 }
 </script>
 
-<style lang="scss">
-@import url(https://fonts.googleapis.com/css?family=Roboto:300);
-
-$color-primary: #3498db;	
-$color-secondary: #f1c40f;	
-
-html, body{
-  padding: 0;
-  margin: 0;
-  font-family: Roboto;
-  background-color: #eee;
-}
-
-section{
-  position: relative;
-  width: 80%;
-  background-color: #fff;  
-  box-shadow: 0px 2px 10px rgba(0,0,0,.3);
-  overflow: hidden;
-  margin: 20px auto;
-  border-radius: 2px;
-}
-
-.header{
-  width: 100%;
-  height: 15%;
-  background-color: $color-primary;  
-  overflow: hidden;
-  
-  .circle{
-    position: relative;
-    top: 20px;
-    left: 120px;
-    width: 50px;
-    height: 50px;
-    background-color: $color-secondary;    
-    box-shadow: 0px 3px 3px rgba(0,0,0,.3);
-    border-radius: 50%;
-    z-index: 10;
-  }
-  
-  h1{
-    position: relative;
-    top: 10px;
-    left: 150px;
-    font-size: 2.5em;
-    color: #fff;
-    border-bottom: 1px solid $color-secondary;
-  }
-}
-
-.line{
-  position: absolute;
-  z-index: 0;
-  top: 50px;
-  left: 145px;
-  height: calc(100% + 200px);
-  width: 0px;
-  border-right: 1px solid $color-secondary;
-}
-
-#content{
-  width: 100%;
-  height: 80%;
-  overflow: hidden;
-  padding-top: 20px;
-  overflow-y: auto;
-  overflow-x: hidden;
-  
-  &::-webkit-scrollbar {
-    width: 6px;
-    background-color: #EBEBEB;  
-  }
-
-  &::-webkit-scrollbar-thumb {
-    background-color: #ccc; 
-  }
-  
-  .object{
-    width: 100%;
-    &:after{
-      content: "";
-      display: block; 
-      height: 0; 
-      clear: both;
-    }
-    .date{
-      float: left;
-      width: 100px;
-      height: 40px;
-      margin: 20px;
-      background-color: $color-secondary;
-      color: #fff;
-      border-radius: 10px;
-      text-align: center;
-      line-height: 1.9em;
-      font-size: 1.1em;
-    }
-    .circle{
-      position: relative;
-      float: left;
-      z-index: 10;
-      margin: 30px 0 0 -5px;
-      width: 20px;
-      height: 20px;
-      background-color: $color-primary;
-      border-radius: 50%;
-    }
-    .context{
-      float: left;
-      color: #666;
-      width: 60%;
-      min-height: 40px;
-      margin: 10px 0 0px 0px;
-      padding: 10px 0px 0px 20px;
-      line-height: 1.5em;
-    }
-  }
-}
-</style>
---
+++ src/webservices/Noticia.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="object my-3">
+    <div class="card">
+      <div class="row text-center mt-2">
+        <div class="col-12"><img class='imagen rounded' :src='imagen ? imagen:imagenDefecto' style="width:75%"/></div>
+      </div>
+      <div class="card-body">
+        <b class="my-2">{{titulo}}<br/>{{fecha}}</b>
+        <p>{{detalle}} Fuente: {{fuente}}</p>
+        <a v-if='url' :href='url' target='_blank' >Leer detalle...</a>
+      </div>
+    </div>
+
+  </div>
+  
+</template>
+
+<script>
+//import moment from 'moment'
+
+export default {
+  name: 'noticia-componente',
+  props: ['fecha','imagen','titulo','detalle','url','fuente'],
+  data: function () {
+    return {
+      imagenDefecto: "http://icons.iconarchive.com/icons/pelfusion/long-shadow-ios7/128/News-icon.png"}
+  },
+  /* 
+  filters: {
+    formatoFecha: function (fecha) {
+     moment.locale(navigator.language);
+     return moment(fecha).format('dddd, D MMMM hh:mm:ss')
+    }
+  }
+  */
+}
+</script>
